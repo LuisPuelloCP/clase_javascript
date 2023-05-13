34ea942c4265d45b433fbec693b3015868baaf87
Luis Puello <luiscapupa@gmail.com>
2023-05-13 10:20:04 -0500

    coments and variables

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-alert("Hello world")
+//Hello world
+//alert("Hello world")
+
+//Line comments
+/*
+    First comment
+    Second comment
+    Third comment
+*/
+
+//Statements
+/*alert("Hello")
+alert("World!")*/
+
+
+// Variables
+let a = 5;
+let b = 6;
+
+let sum = a + b;
+
+console.log("The total sum is: ", sum);
+
+let name = "Luis";
+let name_two = "Camilo";
+
+console.log("The first name is: ", name);
+console.log("The second name is: ", name_two);
+
+// Variables Notations
+
+let camelCase = 5;
+let snake_case = 5;
+let Pascal_Case = 5;
+
+let full_name = "Luis Camilo";
+
+/* 
+    Can't not start with number
+    Can not 
+*/
+
+// Constants
+
+const PI = 3.1416;
+const COLOR = "Red";
